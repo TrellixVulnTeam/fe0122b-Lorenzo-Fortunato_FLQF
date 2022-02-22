1fb09e54192540e07d4b87c1de2877484edbbfeb
lorenzo-fortunato <lorenzcapoeira@gmail.com>
2022-02-22 17:27:37 +0100

    whilewhile

--- s5/g2/js/script.js
+++ s5/g2/js/script.js
@@ -8,11 +8,18 @@ function randomico(){
 }
 
 
-
 while(budgetIniziale > 0){
     var sp = randomico();
-    document.getElementById('spesa').innerHTML += 'spese' + ' ' + sp + '<br>';
+    document.getElementById('spesa').innerHTML += 'ti restano' + ' ' +  budgetIniziale +  ' ' + 'e hai avuto spese per' + ' ' + sp + '<br>';
     budgetIniziale -= sp
+    
+    if (budgetIniziale < 500 && budgetIniziale > 420){
+        document.getElementById('spesa').innerHTML += 'sei arrivato a metà delle tue stupide disponibilità' + '<br>';
+    } 
+   
+    if (budgetIniziale < 200){
+        document.getElementById('spesa').innerHTML += 'STAI FINENDO I CASHI!' + '<br>';
+    }
     
     
 }
