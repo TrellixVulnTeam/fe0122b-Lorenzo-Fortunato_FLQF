58b36e53e6f61c3c32c16ee6cc36ca0f358f9045
lorenzo-fortunato <lorenzcapoeira@gmail.com>
2022-02-23 20:14:03 +0100

    s5g3 quasi finito

--- s5/g3/js/script.js
+++ s5/g3/js/script.js
@@ -21,16 +21,61 @@ btn.addEventListener("click", giovanni);
 
 
 //rosso
+var red = document.querySelector('#rosso');
+red.addEventListener('click', function(){
+    var lorem = document.querySelector('#testo');
+    if (lorem.style.color == ""){
+        lorem.style.color = "red";
+    } else {
+        lorem.style.color = "";
+    }
+});
 
-var rosso = document.getElementById('rosso');
 
-function coloreRosso {
+//grande
+var big = document.querySelector('#grande');
+big.addEventListener('click', function(){
+    var lorem = document.querySelector('#testo');
+    if (lorem.style.textTransform == ""){
+        lorem.style.textTransform = "uppercase";
+    } else {
+        lorem.style.textTransform = "";
+    }
+});
+
+//bgblu
+var bg = document.querySelector('#bgblu');
+bg.addEventListener('click', function(){
+    var lorem = document.querySelector('#testo');
+    if (lorem.style.backgroundColor == ""){
+        lorem.style.backgroundColor = "blue";
+        lorem.style.color = "white";
+    } else {
+        lorem.style.backgroundColor = "";
+        lorem.style.color = "";
+    }
+});
 
 
+//lista depennata
+/*lista.addEventListener('click', function(){
+var lista = document.getElementsByClassName('day');
+if (lista.style.color == ""){
+    lista.style.color = "red"
+} else {
+    lista.style.color = ""
+    }
+})*/
 
-
-}
-
+var list = document.querySelector('.day');
+list.addEventListener('click', function(){
+    var list = document.querySelector('.day');
+    if (list.style.color == ""){
+        list.style.color = "red";
+    } else {
+        list.style.color = "";
+    }
+});
 
 
 
