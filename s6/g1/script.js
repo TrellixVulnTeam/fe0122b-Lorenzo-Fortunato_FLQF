555833ebe3a37ef17b67edcb4a42f91152c67bec
lorenzo-fortunato <lorenzcapoeira@gmail.com>
2022-02-28 17:17:32 +0100

    s6g1 finito

---
+++ s6/g1/script.js
@@ -0,0 +1,66 @@
+// Quando il documento è pronto...
+$(document).ready( () => {
+
+
+    $('#iscr-news').click( () => {
+        
+        let check = $('#nome').val();
+        if (check == ""){
+            $("#nome").next().text('* richiesto');
+        }else{
+            $("#nome").next().text('bravo');
+        }
+    
+        let email1 = $('#email-1').val();
+        if (email1 == ""){
+            $("#email-1").next().text('* richiesto');
+        }else{
+            $("#email-1").next().text('bravo');
+        }
+
+        let email2 = $('#email-2').val();
+        if (email2 == ""){
+            $("#email-2").next().text('* richiesto');
+        }else{
+            $("#email-2").next().text('bravo');
+        }
+        
+        if (email1 != email2){
+            alert('Email diverse!');
+        }else if(check != '' && email1 != '' && email2 != ''){
+            alert('Ti sei iscritto!');
+            
+        }
+
+        
+        
+    })
+    
+    $('#pulisci-form').click( () => {
+        $('#nome, #email-1, #email-2').val('');
+    })
+    
+
+
+
+
+});
+
+
+        
+
+
+
+
+
+
+
+       
+        
+        
+        
+
+        
+
+
+    //posizionare un asterisco per identificare il campo obbligatorio. 
