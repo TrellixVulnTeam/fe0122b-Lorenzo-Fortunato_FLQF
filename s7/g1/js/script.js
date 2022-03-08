8cc7b2adb2a66a398ee4f6cb20c37a8de500d93c
lorenzo-fortunato <lorenzcapoeira@gmail.com>
2022-03-08 17:48:04 +0100

    s7g1 rifatto

--- s7/g1/js/script.js
+++ s7/g1/js/script.js
@@ -1,115 +1,77 @@
-let bottoneInvia = document.querySelector('.bottone');
+//////// FORM ///////////
 
-bottoneInvia.addEventListener('click', function(e){
+let bottone = document.querySelector('.bottone');
+let input = document.querySelectorAll('.val');
+bottone.addEventListener('click', function(e) {
     e.preventDefault();
-    var arrayy = document.querySelectorAll('.val')
-
-    arrayy.forEach(elemento =>{
-        let valore = elemento.value;
-        if(valore == ""){
-            elemento.style.border = '1px solid red';
-        }else if(psw1.value != psw2.value){
-            let psw1 = document.querySelector('#psw1');
-            let psw2 = document.querySelector('#psw2');
-            psw1.style.border = '1px solid red';
-            psw2.style.border = '1px solid red';
-        }else{
-            elemento.style.border = '';
-            
-        }
-        
-
-    });
-
-    
     let nome = document.querySelector('#nome').value;
     let cognome = document.querySelector('#cognome').value;
     let user = document.querySelector('#user').value;
     let email = document.querySelector('#email').value;
-    let password1 = document.querySelector('#psw1').value;
-    let password2 = document.querySelector('#psw2').value;
     
-    let layer = document.querySelector('#layer');
-    if(nome != "" && cognome != "" && email != "" && password1 != "" && password2 != "" && password1 == password2){
-        layer.style.display = 'block';
-        salva();
-        let nome = document.querySelector('#nome').value = "";
-        let cognome = document.querySelector('#cognome').value = "";
-        let user = document.querySelector('#user').value = "";
-        let email = document.querySelector('#email').value = "";
-        let password1 = document.querySelector('#psw1').value = "";
-        let password2 = document.querySelector('#psw2').value = "";
-    }
-
-    if(password1 != password2){
-        password1.style.border = '1px solid red';
-        password2.style.border = '1px solid red';
-    }
-
-    let chiudi = document.querySelector('#chiudi');
-    chiudi.addEventListener('click', () => {
-        layer.style.display = 'none';
-
-    });
-
-    
-
-
-    /*if(x > 0 ){
-        let popup = document.querySelector('#popup');
-        popup.style.display = 'block';
-        popup.style.zIndex = '100';
-        forM.style.display = 'none';
-        
-    }
-    
-    let chiudi = document.querySelector('#chiudi');
-    console.log(chiudi)
-    chiudi.addEventListener('click', () => {
-        popup.style.display = 'none';
-        forM.style.display = 'block';
-    })*/
-
-  
-    function salva(){
-        class Utente {
-            constructor (nome, cognome, user, email, password1, password2){
-                this.nome = nome;
-                this.cognome = cognome;
-                this.user = user;
-                this.email = email;
-                this.password1 = password1;
-                this.password2 = password2;
-            };
-        };
-
-        
-
-        const persona = new Utente(nome, cognome, user, password1, password2);
-     
-        let list = localStorage.getItem('persona');
-        
-        let tern = list == null ? [] : JSON.parse(list);
-        tern.push(persona);
-
-        let json = JSON.stringify(tern);
-        localStorage.setItem('persona', json);
-
-        
+        input.forEach(elemento => {
+            let inputValue = elemento.value;
+            if (inputValue == ""){
+                elemento.style.border = "1px solid red";
+            }else{
+                elemento.style.border = "";
+            }
+        });
+    let password1 = document.querySelector('#psw1');
+    let password2 = document.querySelector('#psw2');
+    // console.log(password1.value)
+    if(password1.value != password2.value){
+        password1.style.border = "1px solid orange";
+        password2.style.border = "1px solid orange";
     };
+    if(nome != "" && cognome != "" && user != "" && email != "" && password1.value == password2.value){
+        layer.style.display = 'block';
+        const persona = new Utente(nome, cognome, user, password1.value, password2.value);
+        salva(persona);
+    };
+
+
+});
+
+let chiudi = document.querySelector('#chiudi');
+chiudi.addEventListener('click', () => {
+    layer.style.display = 'none';
+    input.forEach(elemento1 => {
+        elemento1.value = "";
+    })
+});
+
+// classe utente
+class Utente {
+    constructor (nome, cognome, user, email, password1, password2){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.user = user;
+        this.email = email;
+        this.password1 = password1;
+        this.password2 = password2;
+    };
+};
+
+function salva(pippo){
+    let list = localStorage.getItem('lista');
+    let tern = list == null ? [] : JSON.parse(list);
+    tern.push(pippo);
+    let json = JSON.stringify(tern);
+    localStorage.setItem('lista', json);
+
     
-    
+};
 
 
 
 
-    
-
-    
-    
 
 
-})
+
+// NON CI SI PUò ISCRIVERE DUE VOLTE CON LA STESSA EMAIL!!!
+
+//////// LOG-IN ///////////
 
 
 
@@ -120,15 +82,23 @@ bottoneInvia.addEventListener('click', function(e){
 
 
 // FORM
-    // 1. required e annessi
-        // 1.1 campi required
-        // 1.2 match password
-        // 1.3 tutti i campi sono necessari
+    // 1. required e annessi --> OK
+        // 1.1 campi required --> OK 
+        // 1.2 match password --> OK
+        // 1.3 tutti i campi sono necessari --> OK
 
-    // 2. al click localStorage
-        // 2.1 al click crea un oggetto composto dai value degli input (OOP)
-        // 2.2 creare array cestino
-        // !!!! non creare utenti vuoti! !!!!!!
-        // 2.3 pushare gli oggetti nell'array cestino
+    // 2. al click localStorage --> OK
+        // 2.1 al click crea un oggetto composto dai value degli input (OOP) --> OK
+        // 2.2 creare array cestino --> OK
+        // !!!! non creare utenti vuoti! !!!!!! --> OK
+        // 2.3 pushare gli oggetti nell'array cestino --> OK
     
 // extras: lettere e simboli ammessi negli input
+
+
+// LOG-IN
+    // 1. inserimento dati 
+        // 1.1 i campi non devono essere lasciati vuoti
+        // 1.2 crea un oggetto per ogni inserimento
+        // 1.3 compara l'oggetto log-in con l'oggetto form
+     
