521fa47ba389b97250398fc82a998620307a05e3
lorenzo-fortunato <lorenzcapoeira@gmail.com>
2022-03-14 18:01:00 +0100

    s8g1 finito2

--- s8/data.js
+++ s8/data.js
@@ -1,25 +1,31 @@
 var giocatore1 = 10;
+var gioco1 = document.querySelector('#gioco1');
+gioco1.innerText = "Il giocatore 1 ha scelto il numero: " + giocatore1;
 var giocatore2 = 70;
+var gioco2 = document.querySelector('#gioco2');
+gioco2.innerText = "Il giocatore 2 ha scelto il numero: " + giocatore2;
 function caos() {
     var random = Math.floor(Math.random() * (100 - 1) + 1);
-    console.log(random);
+    var randomico = document.querySelector('#random');
+    randomico.innerText = "Il numero estratto è: " + random;
+    // console.log(random);
+    var final = document.querySelector('#finale');
     if (random == giocatore1) {
-        console.log('Complimenti giocatore1, hai vinto!');
+        final.innerText = "Complimenti giocatore1, hai vinto!";
     }
     else if (random == giocatore2) {
-        console.log('Complimenti giocatore2, hai vinto!');
+        final.innerText = "Complimenti giocatore2, hai vinto!";
     }
     else {
         var sottr1 = Math.abs(random - giocatore1);
         var sottr2 = Math.abs(random - giocatore2);
-        console.log(sottr1);
-        console.log(sottr2);
-        // console.log(sottrai1);
+        // console.log(sottr1);
+        // console.log(sottr2);
         if (sottr1 < sottr2) {
-            console.log('Mannaggia! il giocatore1 non ha indovinato il numero ma comunque è quello che si è avvicinato di più!');
+            final.innerText = 'Mannaggia! Il giocatore1 non ha indovinato il numero ma comunque è quello che si è avvicinato di più!';
         }
         else {
-            console.log('Puffappero! il giocatore2 non ha indovinato il numero ma comunque è quello che si è avvicinato di più!');
+            final.innerText = 'Puffappero! Il giocatore2 non ha indovinato il numero ma comunque è quello che si è avvicinato di più!';
         }
         ;
     }
--- s8/data.ts
+++ s8/data.ts
@@ -1,25 +1,31 @@
 let giocatore1: number = 10;
+let gioco1: any = document.querySelector('#gioco1');
+gioco1.innerText = "Il giocatore 1 ha scelto il numero: " + giocatore1;
 
 let giocatore2: number = 70;
+let gioco2: any = document.querySelector('#gioco2');
+gioco2.innerText = "Il giocatore 2 ha scelto il numero: " + giocatore2;
 
 function caos(){
     let random: number = Math.floor(Math.random() * (100-1) + 1);
-    console.log(random);
+    let randomico: any = document.querySelector('#random');
+    randomico.innerText = "Il numero estratto è: " + random;
+    // console.log(random);
 
+    let final: any = document.querySelector('#finale');
     if(random == giocatore1){
-        console.log('Complimenti giocatore1, hai vinto!');
+        final.innerText = "Complimenti giocatore1, hai vinto!";
     }else if(random == giocatore2){
-        console.log('Complimenti giocatore2, hai vinto!');
+        final.innerText = "Complimenti giocatore2, hai vinto!";
     }else{
         let sottr1: number = Math.abs(random-giocatore1);
         let sottr2: number = Math.abs(random-giocatore2);
-        console.log(sottr1);
-        console.log(sottr2);
-        // console.log(sottrai1);
+        // console.log(sottr1);
+        // console.log(sottr2);
             if(sottr1 < sottr2){
-                console.log('Mannaggia! il giocatore1 non ha indovinato il numero ma comunque è quello che si è avvicinato di più!');
+                final.innerText = 'Mannaggia! Il giocatore1 non ha indovinato il numero ma comunque è quello che si è avvicinato di più!';
             }else{
-                console.log('Puffappero! il giocatore2 non ha indovinato il numero ma comunque è quello che si è avvicinato di più!');
+                final.innerText = 'Puffappero! Il giocatore2 non ha indovinato il numero ma comunque è quello che si è avvicinato di più!';
             };
             
     };
